5b30550827e1209bb1452b9f12d34b0c5dee9e20
Olwin1 <oliver_munn@icloud.com>
2021-10-23 13:48:40 +0100

    Change Button Link If Logged In Or Not

--- src/client/Components/Login/NavLogin.tsx
+++ src/client/Components/Login/NavLogin.tsx
@@ -6,15 +6,19 @@ const NavLogin = () => {
   };
   if (getCookie("token") != undefined) {
     return (
+		<a className="navbar-item" href="/login">
 		<button className="button is-info is-outlined" style={divStyle}>
         Go To Dashboard
       </button>
+	  </a>
     );
   }
   return (
+	<a className="navbar-item" href="https://discord.com/api/oauth2/authorize?client_id=764794183083884546&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Foauth%2Fredirect&response_type=token&scope=identify%20guilds%20guilds.join">
     <button className="button is-info is-outlined" style={divStyle}>
       Login
     </button>
+	</a>
   );
 };
 
