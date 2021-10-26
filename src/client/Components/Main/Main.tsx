451991bba5057d63a0069969d437a7109d9379e6
Olwin1 <oliver_munn@icloud.com>
2021-10-26 21:49:57 +0100

    Added Component Switching.

--- src/client/Components/Main/Main.tsx
+++ src/client/Components/Main/Main.tsx
@@ -1,17 +1,28 @@
-import React from "react";
-import { FC } from "react"
+import React, { FC,useState } from "react";
+import * as modules from "./Modules";
 
 interface TyProps {
   user:JSON
 }
 
 const Main: FC<TyProps> = (props) => {
+  const [component, setComponent] = useState("Overview");
   return (
     <div>
+      {component === 'Overview' && <modules.Overview />}
+      {component === 'Settings' && <modules.Settings />}
       <p>Dis Is Da Main LOlolololololol</p>
       <p>{JSON.stringify(props.user)}</p>
+      <button onClick={() => setComponent("Settings")}>Settings</button>
+      <button onClick={() => setComponent("Overview")}>Overview</button>
       </div>
   )
 };
+const Test1 = () => {
+  return(<p>Test1</p>)
+}
+const Test2 = () => {
+  return(<p>Test2</p>)
+}
 
 export default Main;
---
+++ src/client/Components/Main/Modules/Overview.tsx
@@ -0,0 +1,17 @@
+import React, { FC,useState } from "react";
+
+
+//interface TyProps {
+//  user:JSON
+//}
+
+const Main/*: FC<TyProps>*/ = () => {
+  return (
+    <div>
+      <p>Overview</p>
+      </div>
+  )
+};
+
+
+export default Main;
