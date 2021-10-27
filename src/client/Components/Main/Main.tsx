d7b2db6f00a5cf436ab295d220b801d515f1c730
Olwin1 <oliver_munn@icloud.com>
2021-10-27 12:01:08 +0100

    Component Change On Hash Change

--- src/client/Components/Main/Main.tsx
+++ src/client/Components/Main/Main.tsx
@@ -1,28 +1,41 @@
-import React, { FC,useState } from "react";
+import React, { FC, useState } from "react";
 import * as modules from "./Modules";
 
 interface TyProps {
-  user:JSON
+  user: JSON;
 }
 
 const Main: FC<TyProps> = (props) => {
   const [component, setComponent] = useState("Overview");
+
+  const locationHashChanged = () => {
+    setComponent(window.location.hash);
+  };
+
+  window.onhashchange = locationHashChanged;
+
   return (
     <div>
-      {component === 'Overview' && <modules.Overview />}
-      {component === 'Settings' && <modules.Settings />}
-      <p>Dis Is Da Main LOlolololololol</p>
-      <p>{JSON.stringify(props.user)}</p>
-      <button onClick={() => setComponent("Settings")}>Settings</button>
-      <button onClick={() => setComponent("Overview")}>Overview</button>
-      </div>
-  )
+      {component.toLowerCase() === "#overview" && <modules.Overview user={props.user}/>}
+      {component.toLowerCase() === "#settings" && <modules.Settings />}
+      {component.toLowerCase() === "#embed" && <modules.Settings />}
+
+      {component.toLowerCase() === "#moderation" && <modules.Settings />}
+      {component.toLowerCase() === "#automod" && <modules.Settings />}
+      {component.toLowerCase() === "#welcomer" && <modules.Settings />}
+      {component.toLowerCase() === "#autoresponder" && <modules.Settings />}
+      {component.toLowerCase() === "#leveling" && <modules.Settings />}
+      {component.toLowerCase() === "#autorole" && <modules.Settings />}
+      {component.toLowerCase() === "#reeactionroles" && <modules.Settings />}
+      {component.toLowerCase() === "#music" && <modules.Settings />}
+
+
+      {component.toLowerCase() === "#modlog" && <modules.Settings />}
+
+
+
+    </div>
+  );
 };
-const Test1 = () => {
-  return(<p>Test1</p>)
-}
-const Test2 = () => {
-  return(<p>Test2</p>)
-}
 
 export default Main;
--- src/client/Components/Main/Modules/Overview.tsx
+++ src/client/Components/Main/Modules/Overview.tsx
@@ -1,14 +1,15 @@
 import React, { FC,useState } from "react";
 
 
-//interface TyProps {
-//  user:JSON
-//}
+interface TyProps {
+  user:JSON
+}
 
-const Main/*: FC<TyProps>*/ = () => {
+const Main: FC<TyProps> = (props) => {
   return (
     <div>
       <p>Overview</p>
+      <p>{JSON.stringify(props.user)}</p>
       </div>
   )
 };
