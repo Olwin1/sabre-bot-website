780cdddbf92c7520791c63d4b386931fd85790b0
Olwin1 <oliver_munn@icloud.com>
2021-10-30 18:55:20 +0100

    Added Toggle Hook

---
+++ src/client/Components/Main/Modules/hooks/toggle.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function useToggle(initialValue = false) {
+    const [value, setValue] = React.useState(initialValue);
+    const toggle = () => {
+        setValue(!value)
+    }
+    return [value, toggle] as const;
+  }
