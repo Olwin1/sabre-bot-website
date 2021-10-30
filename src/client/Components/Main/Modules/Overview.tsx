6de64621296410a4045b43950cf71c2159952a91
Olwin1 <oliver_munn@icloud.com>
2021-10-30 18:56:12 +0100

    Added On/Off Button Using Toggle Hook & Basic Card

--- src/client/Components/Main/Modules/Overview.tsx
+++ src/client/Components/Main/Modules/Overview.tsx
@@ -1,17 +1,48 @@
-import React, { FC,useState } from "react";
-
-
+import React, { FC, useState } from "react";
+import useToggle from "./hooks/toggle";
 interface TyProps {
-  user:JSON
+  user: JSON;
+}
+interface BtnProps {
+  type: string;
 }
 
 const Main: FC<TyProps> = (props) => {
+  const [isOn, toggleIsOn] = useToggle();
+
+  const Content: FC<TyProps> = (props) => {
+
+    const Button: FC<BtnProps> = (props) => {
+      if(isOn) {
+        return(
+          <button onClick={() => toggleIsOn()}>On</button>
+        )
+      }
+      else {
+        return(
+          <button onClick={() => toggleIsOn()}>Off</button>
+        )
+      }
+    }
+    return (
+      <div>
+        <h1 className="title is-4">List Of Available Modules</h1>
+        <p>What Modules Do You Want To Use?</p>
+
+        <div className="is-card">
+          <h1 className="title is-5">Leveling</h1>
+          <p className="subtitle is-6 is-indented">USe This To Lvlup</p>
+          <Button type="" />
+        </div>
+        <p>{JSON.stringify(props.user)}</p>
+      </div>
+    );
+  };
   return (
     <div>
-      <p>Overview</p>
-      <p>{JSON.stringify(props.user)}</p>
-      </div>
-  )
+      <Content user={props.user} />
+    </div>
+  );
 };
 
 
