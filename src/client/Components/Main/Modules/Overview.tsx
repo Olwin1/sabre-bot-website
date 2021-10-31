15a2864ba6d67a3e453b0524840fc71e84e0b3d9
Olwin1 <oliver_munn@icloud.com>
2021-10-31 10:55:32 +0000

    Makes Api Request On Click Of Slider

--- src/client/Components/Main/Modules/Overview.tsx
+++ src/client/Components/Main/Modules/Overview.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useState } from "react";
 import useToggle from "./hooks/toggle";
+
 interface TyProps {
   user: JSON;
 }
@@ -11,16 +12,28 @@ interface BtnProps {
 }
 
 const Main: FC<TyProps> = (props) => {
-
   const Content: FC<CardProps> = (props) => {
-
-
     const Button: FC<BtnProps> = (props) => {
       let toggle = false;
-      const t = (type:string) => {
+      const t = (type: string) => {
         const x = document.getElementById("btn" + type);
         const y = document.getElementById("handle" + type);
         toggle = !toggle;
+
+        //Send Click Data To API.
+        const apiGet = async (type:string, toggle:boolean) => {
+            y.classList.add("lds-ring");
+            return window.fetch("https://jsonplaceholder.typicode.com/users/1")
+              .then(response => {
+                if (!response.ok) {
+                  throw new Error(response.statusText)
+                }
+                y.classList.remove("lds-ring");
+                return response.json()
+              })
+        };
+        apiGet(props.type, toggle);
+
         if (toggle) {
           x.classList.add("is-ToggleButtonA");
           y.classList.add("handleA");
@@ -31,11 +44,19 @@ const Main: FC<TyProps> = (props) => {
       };
 
       return (
-        <button id={"btn" + props.type} className={!toggle ? "is-ToggleButton" : "is-ToggleButton is-ToggleButtonA"} onClick={() => t(props.type)}>
+        <button
+          id={"btn" + props.type}
+          className={
+            !toggle ? "is-ToggleButton" : "is-ToggleButton is-ToggleButtonA"
+          }
+          onClick={() => t(props.type)}
+        >
           <div
             id={"handle" + props.type}
-            className={!toggle ? "handle lds-ring" : "handle lds-ring handleA"}
-          ><div></div></div>
+            className={!toggle ? "handle" : "handle handleA"}
+          >
+            <div></div>
+          </div>
         </button>
       );
     };
@@ -43,10 +64,6 @@ const Main: FC<TyProps> = (props) => {
       <div>
         <h1 className="title is-4">List Of Available Modules</h1>
         <p>What Modules Do You Want To Use?</p>
-        
-
-
-
 
         <div className="is-card">
           <div className="float-right">
@@ -56,7 +73,6 @@ const Main: FC<TyProps> = (props) => {
           <p className="subtitle is-6 is-indented">Change Your Leveles</p>
         </div>
 
-
         <div className="is-card">
           <div className="float-right">
             <Button type="Moderation" />
@@ -64,12 +80,6 @@ const Main: FC<TyProps> = (props) => {
           <h1 className="title is-5">Moderation</h1>
           <p className="subtitle is-6 is-indented">Do Mod Stuff.</p>
         </div>
-
-
-
-
-
-
 
         <p>{JSON.stringify(props.user)}</p>
       </div>
