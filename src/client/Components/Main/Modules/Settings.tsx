86ae1e4dd2851086281f3ee944dee371b21e3120
Olwin1 <oliver_munn@icloud.com>
2021-11-10 19:27:39 +0000

    Added Basic Language Option For Options

--- src/client/Components/Main/Modules/Settings.tsx
+++ src/client/Components/Main/Modules/Settings.tsx
@@ -1,17 +1,40 @@
-import React, { FC,useState } from "react";
+import React, { FC, useState } from "react";
+import { ProgressPlugin } from "webpack";
 
-
-//interface TyProps {
-//  user:JSON
-//}
-
-const Main/*: FC<TyProps>*/ = () => {
+interface TyProps {
+  user: JSON;
+}
+interface CardProps {
+  data: JSON;
+}
+const Card: FC<CardProps> = (props) => {
   return (
-    <div>
-      <p>Settings</p>
+    <div className="float-right is-padded">
+      <div className="select is-rounded is-primary">
+        <select name="languages" id="languages">
+          <option value="english">English</option>
+        </select>
       </div>
-  )
+    </div>
+  );
 };
 
+const Main: FC<TyProps> = (props) => {
+  return (
+    <div>
+      <h1 className="title is-4">General Options</h1>
+      <div className="is-card">
+        <div className="columns">
+          <div className="column">
+            <p>Languages</p>
+          </div>
+          <div>
+            <Card data={props.user} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Main;
