5adef599bc6524249637ff66cc700bc21f00d5e2
Olwin1 <oliver_munn@icloud.com>
2021-11-10 21:09:45 +0000

    Added Moderation Module

---
+++ src/client/Components/Main/Modules/Moderation.tsx
@@ -0,0 +1,121 @@
+import React, { FC, useState } from "react";
+import useToggle from "./hooks/toggle";
+
+interface TyProps {
+  user: JSON;
+}
+interface CardProps {
+  data: JSON;
+}
+interface BtnProps {
+  type: string;
+}
+const Card: FC<CardProps> = (props) => {
+  return (
+    <div className="float-right is-padded">
+      <div className="select is-rounded is-primary">
+        <select name="languages" id="languages">
+          <option value="english">English</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+const Main: FC<TyProps> = (props) => {
+  const [pane, setPane] = useToggle();
+  const Button: FC<BtnProps> = (props) => {
+    let toggle = false;
+    const t = (type: string) => {
+      const x = document.getElementById("btn" + type);
+      const y = document.getElementById("handle" + type);
+      toggle = !toggle;
+
+      //Send Click Data To API.
+      const apiGet = async (type: string, toggle: boolean) => {
+        y.classList.add("lds-ring");
+        return window
+          .fetch("https://jsonplaceholder.typicode.com/users/1")
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error(response.statusText);
+            }
+            y.classList.remove("lds-ring");
+            return response.json();
+          });
+      };
+      apiGet(props.type, toggle);
+
+      if (toggle) {
+        x.classList.add("is-ToggleButtonA");
+        y.classList.add("handleA");
+      } else {
+        x.classList.remove("is-ToggleButtonA");
+        y.classList.remove("handleA");
+      }
+    };
+    return (
+      <button
+        id={"btn" + props.type}
+        className={
+          !toggle ? "is-ToggleButton" : "is-ToggleButton is-ToggleButtonA"
+        }
+        onClick={() => t(props.type)}
+      >
+        <div
+          id={"handle" + props.type}
+          className={!toggle ? "handle" : "handle handleA"}
+        >
+          <div></div>
+        </div>
+      </button>
+    );
+  };
+
+  return (
+    <div>
+      <h1 className="title is-4">Moderation</h1>
+      <div className="is-card">
+        <div className={pane ? "is-blocked" : ""}>f</div>
+        <div className="columns">
+          <div className="column">
+            <button onClick={() => setPane()}>Click for tioggle</button>
+          </div>
+          <div>
+            <Button type="toggle_moderation" />
+          </div>
+        </div>
+
+        <h2 className="title is-5">Helper Role</h2>
+            <p className="subtitle is-6">
+              The Most Basic Of Moderation Commands. In Order To Use Commands
+              Such as Warn and Mute You Will Need To Have The Role Specified
+              Here.
+            </p>
+            <div className="select is-rounded is-primary">
+              <select name="languages" id="languages">
+                <option value="helper">Helper</option>
+              </select>
+            </div>
+            <br />
+            <br />
+            <hr />
+            <h2 className="title is-5">Mod Role</h2>
+            <p className="subtitle is-6">
+              The More Powerful Moderation Commands. Commands Such as Bans,
+              Channel Locks and Kicks Will Only Be Available To Members With
+              This Role.  
+            </p>
+            <div className="select is-rounded is-primary">
+              <select name="languages" id="languages">
+                <option value="moderator">Moderator</option>
+              </select>
+            </div>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Main;
