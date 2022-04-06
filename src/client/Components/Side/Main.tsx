4a262d7a61293a7153e12dd58368e1c5a7f697f0
Olwin1 <oliver_munn@icloud.com>
2022-04-06 15:21:56 +0100

    Moved Options Sidebar to Own Component

--- src/client/Components/Side/Main.tsx
+++ src/client/Components/Side/Main.tsx
@@ -11,12 +11,23 @@ interface SideProp {
 interface GProp {
   uid: string,
 }
+interface UProp {
+  uid: string,
+}
 const Subitem: FC<TyProp> = (props): JSX.Element => {
   return (
     <li>
       <a href={"#" + props.link} className="no-select">
         {props.text}
       </a>
+    </li>
+  );
+};
+const Useritem: FC<UProp> = (props): JSX.Element => {
+  return (
+    <li>
+      <img src={"https://www.jing.fm/clipimg/full/13-136405_clipart-big-image-default-user-icon-png.png"} width="64" className="no-select bordered">
+      </img>
     </li>
   );
 };
@@ -34,38 +45,47 @@ const Sidebar: FC<SideProp> = (props) => {
     <div className="columns">
       <div className="is-tall column is-72px">
       <ul className="menu-list serverList">
+        <Useritem uid="416617058248425473" />
+        <hr />
         <Guilditem uid="704255331680911402" />
 
       </ul>
 
     </div>    
-    <div className="is-tall column">
-      <p>Dashboard</p>
-      <ul className="menu-list">
-        <Subitem link="overview" text="Overview" />
-        <Subitem link="settings" text="Settings" />
-        <Subitem link="embed" text="Embed" />
 
-        <br />
+    <SidebarOptions />
 
-        <Subitem link="moderation" text="Moderation" />
-        <Subitem link="automod" text="Auto-Mod" />
-        <Subitem link="welcomer" text="Welcomer" />
-        <Subitem link="autoresponder" text="Auto-responder" />
-        <Subitem link="leveling" text="Leveling" />
-        <Subitem link="autorole" text="Auto-Role" />
-        <Subitem link="reactionroles" text="Reaction Roles" />
-        <Subitem link="music" text="Music" />
-
-        <br /><br />
-
-        <Subitem link="modlog" text="Mod-Log" />
-
-      </ul>
-    </div>
 
     </div>
   );
 };
+const SidebarOptions = () => {
+  return(
+    <div className="is-tall column">
+    <p>Dashboard</p>
+    <ul className="menu-list">
+      <Subitem link="overview" text="Overview" />
+      <Subitem link="settings" text="Settings" />
+      <Subitem link="embed" text="Embed" />
+
+      <br />
+
+      <Subitem link="moderation" text="Moderation" />
+      <Subitem link="automod" text="Auto-Mod" />
+      <Subitem link="welcomer" text="Welcomer" />
+      <Subitem link="autoresponder" text="Auto-responder" />
+      <Subitem link="leveling" text="Leveling" />
+      <Subitem link="autorole" text="Auto-Role" />
+      <Subitem link="reactionroles" text="Reaction Roles" />
+      <Subitem link="music" text="Music" />
+
+      <br /><br />
+
+      <Subitem link="modlog" text="Mod-Log" />
+
+    </ul>
+  </div>
+  )
+}
 
 export default Sidebar;
