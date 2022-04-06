e605bfab99bbdf13ebc9cdf244b21679f9953755
Olwin1 <oliver_munn@icloud.com>
2022-04-06 16:07:46 +0100

    Added Profile Component

--- src/client/Components/Main/Modules/index.ts
+++ src/client/Components/Main/Modules/index.ts
@@ -2,3 +2,4 @@ export { default as Overview } from "./Overview";
 export { default as Settings } from "./Settings";
 export { default as Moderation } from "./Moderation";
 export { default as Embed } from "./Embed";
+export { default as Profile } from "./Profile";
