ecadace6d41f96b69063637bc07cb4dc11fce019
Olwin1 <oliver_munn@icloud.com>
2022-04-09 22:15:43 +0100

    Minor Formatting Changes

--- src/client/User.ts
+++ src/client/User.ts
@@ -1,7 +1,7 @@
 import { getCookie, setCookie } from "./cookie-utils";
 const getUser = () => {
-  const decode = (str: string):string => window.atob(str);
-  const encode = (str: string):string => window.btoa(str);
+  const decode = (str: string): string => window.atob(str);
+  const encode = (str: string): string => window.btoa(str);
   const cookie = getCookie("u");
   if (cookie == undefined) {
     fetch("https://discord.com/api/v9/users/@me", {
@@ -13,10 +13,14 @@ const getUser = () => {
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log('%c [SABRE API] ', 'color: #4E1592', 'User Cache Not Found... Creating...');
-        const temp = String(encode(JSON.stringify(data)))
-        setCookie("u", temp)
-        location.reload()
+        console.log(
+          "%c [SABRE API] ",
+          "color: #4E1592",
+          "User Cache Not Found... Creating..."
+        );
+        const temp = String(encode(JSON.stringify(data)));
+        setCookie("u", temp);
+        location.reload();
       })
       .catch((error) => {
         return error;
