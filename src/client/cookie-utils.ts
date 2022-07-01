69164e75b5762553cac947652db7d68812ef6ab8
Olwin1 <oliver_munn@icloud.com>
2022-07-01 15:55:08 +0100

    Did sum level component shit now tis mostly dun

--- src/client/cookie-utils.ts
+++ src/client/cookie-utils.ts
@@ -15,7 +15,7 @@ export function getCookie(name: string) {
   const parts = value.split("; " + name + "=");
 
   if (parts.length == 2) {
-    return parts.pop().split(";").shift();
+    return parts.pop()?.split(";").shift();
   }
 }
 
